a91c0ed69a482ea21db70bc38c019ae5cf64e84b
larsaars <lurzapps@gmail.com>
2021-09-24 19:44:16 +0200

    use pyperclip to work on all platforms

--- clipboard_utils.py
+++ clipboard_utils.py
@@ -3,29 +3,17 @@ helper to get string from clipboard
 and then return it to there
 """
 
-import win32clipboard
+import pyperclip
 
 # create output string that can later on be appended
 output = []
 
 
 def read_clipboard() -> str:
-    win32clipboard.OpenClipboard()
-    data: str = win32clipboard.GetClipboardData()
-    win32clipboard.CloseClipboard()
-    return data
+    return pyperclip.paste()
 
 
 def write_clipboard():
     global output
     # set output as clipboard data again
-    win32clipboard.OpenClipboard()
-    win32clipboard.EmptyClipboard()
-    output = '\n'.join(output)
-    try:
-        win32clipboard.SetClipboardText(output)
-    except Exception:
-        print(output)
-        input("waiting...")
-
-    win32clipboard.CloseClipboard()
+    pyperclip.copy('\n'.join(output))
